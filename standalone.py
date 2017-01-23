e4299ca413289fa7b5fe5b0f83509a3d0037bb31
Magnus Ödman <magnus@Magnuss-iMac.local>
2017-01-23 07:27:54 +0100

    Added two layer dropout

--- standalone.py
+++ standalone.py
@@ -50,34 +50,45 @@ def rgbToGray(image):
 """
 
 def rgbToGray(rgb):
-    return np.dot(rgb[..., :3], [0.299, 0.587, 0.114]).reshape(32,32,1)
+    return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
 
-"""
-sign_classes = set(y_train)
-for class_index in sign_classes:
-    data_for_class = [index for index in range(len(y_train)) if y_train[index] == class_index]
-    index = data_for_class[0]
-    plt.imshow(rgbToGray(X_train[index]), cmap='gray')
-    plt.title("Traffic sign class =" + str(y_train[index]))
-    plt.xlabel("Count of examples in test data: " + str(len(data_for_class)))
-    plt.show()
-"""
+def add_noise(img):
+    row, col, ch = img.shape
+    mean = 0
+    gauss = np.random.normal(mean,1,(row,col,ch))
+    gauss = gauss.reshape(row,col,ch)
+    noisy = img + gauss
+    return noisy
 
-def pre_process_to_gray(data):
+
+def make_gray(colored_ones):
+    "Convert images to gray"
+    gray_ones = np.zeros((colored_ones.shape[0], colored_ones.shape[1], colored_ones.shape[2], 1)) # init 2D numpy array
+    for imgnum in range(len(colored_ones)):
+        gray_ones[imgnum] = rgbToGray(colored_ones[imgnum]).reshape(32,32,1)
+    return gray_ones
+
+def normalize(img):
+    img *= 255.0/img.max()
+    return img
+
+def pre_process(X_train, y_train):
     "Pre process data converting to gray and normalize"
-    preprocessed = np.zeros((data.shape[0], data.shape[1], data.shape[2],1)) # init 2D numpy array
+    gray_ones = make_gray(X_train)
+    preprocessed = np.zeros((gray_ones.shape[0], gray_ones.shape[1], gray_ones.shape[2], 1)) # init 2D numpy array
     # get row number
     index = 0
-    for imgnum in range(len(data)):
-        preprocessed[imgnum] = rgbToGray(data[imgnum])
+    for imgnum in range(len(gray_ones)):
+        preprocessed[imgnum] = add_noise(normalize(gray_ones[imgnum]))
         index = index + 1
         if index % 1000 == 0:
-            print(str(index) + "/" + str(len(data)))
-    return preprocessed
+            print(str(index) + "/" + str(len(X_train)))
+    return preprocessed, y_train
 
 print("Convert images..")
-X_train = pre_process_to_gray(X_train)
-X_test = pre_process_to_gray(X_test)
+X_train, y_train = pre_process(X_train, y_train)
+X_test = make_gray(X_test)
+
 X_validation = X_test[int(len(X_test)/2):]
 y_validation = y_test[int(len(X_test)/2):]
 y_test = y_test[0:int(len(X_test/2))]
@@ -85,21 +96,36 @@ X_test = X_test[0:int(len(X_test/2))]
 
 print(X_train.shape)
 
+def show_images(X_train, y_train):
+    "Display image examples and number of images"
+    sign_classes = set(y_train)
+    for class_index in sign_classes:
+        data_for_class = [index for index in range(len(y_train)) if y_train[index] == class_index]
+        index = data_for_class[0]
+        img = X_train[index].reshape((32, 32))
+        print(img.shape)
+        plt.imshow(img, cmap='gray')
+        plt.title("Traffic sign class =" + str(y_train[index]))
+        plt.xlabel("Count of examples in test data: " + str(len(data_for_class)))
+        plt.show()
+
+#show_images(X_train, y_train)
+
 from tensorflow.contrib.layers import flatten
 import tensorflow as tf
 
 EPOCHS = 10
 BATCH_SIZE = 128
 
-def LeNet(x):    
+def LeNet(x, keep_prob):
     # Hyperparameters
     mu = 0
     sigma = 0.1
 
     # SOLUTION: Layer 1: Convolutional. Input = 32x32x1. Output = 28x28x6.
-    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 1, 6), mean = mu, stddev = sigma))
+    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 1, 6), mean=mu, stddev=sigma))
     conv1_b = tf.Variable(tf.zeros(6))
-    conv1   = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
+    conv1 = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
 
     # SOLUTION: Activation.
     conv1 = tf.nn.relu(conv1)
@@ -110,7 +136,7 @@ def LeNet(x):
     # SOLUTION: Layer 2: Convolutional. Output = 10x10x16.
     conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean = mu, stddev = sigma))
     conv2_b = tf.Variable(tf.zeros(16))
-    conv2   = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
+    conv2 = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
 
     # SOLUTION: Activation.
     conv2 = tf.nn.relu(conv2)
@@ -121,43 +147,44 @@ def LeNet(x):
     # SOLUTION: Flatten. Input = 5x5x16. Output = 400.
     fc0   = flatten(conv2)
 
+    fc0_drop = tf.nn.dropout(fc0, keep_prob)
+
     # SOLUTION: Layer 3: Fully Connected. Input = 400. Output = 120.
-    fc1_W = tf.Variable(tf.truncated_normal(shape=(400, 120), mean = mu, stddev = sigma))
+    fc1_W = tf.Variable(tf.truncated_normal(shape=(400, 120), mean=mu, stddev=sigma))
     fc1_b = tf.Variable(tf.zeros(120))
-    fc1   = tf.matmul(fc0, fc1_W) + fc1_b
+    fc1 = tf.matmul(fc0_drop, fc1_W) + fc1_b
 
     # SOLUTION: Activation.
-    fc1    = tf.nn.relu(fc1)
+    fc1 = tf.nn.relu(fc1)
+
+    fc1_drop = tf.nn.dropout(fc1, keep_prob)
 
     # SOLUTION: Layer 4: Fully Connected. Input = 120. Output = 84.
-    fc2_W  = tf.Variable(tf.truncated_normal(shape=(120, 84), mean = mu, stddev = sigma))
-    fc2_b  = tf.Variable(tf.zeros(84))
-    fc2    = tf.matmul(fc1, fc2_W) + fc2_b
+    fc2_W = tf.Variable(tf.truncated_normal(shape=(120, 84), mean=mu, stddev=sigma))
+    fc2_b = tf.Variable(tf.zeros(84))
+    fc2 = tf.matmul(fc1_drop, fc2_W) + fc2_b
 
     # SOLUTION: Activation.
-    fc2    = tf.nn.relu(fc2)
-
-    #keep_prob = tf.placeholder(tf.float32)
-    #f2_dropout = tf.nn.dropout(fc2, keep_prob)
+    fc2 = tf.nn.relu(fc2)
 
     # SOLUTION: Layer 5: Fully Connected. Input = 84. Output = 43.
     fc3_W = tf.Variable(tf.truncated_normal(shape=(84, 43), mean=mu, stddev=sigma))
     fc3_b = tf.Variable(tf.zeros(43))
-    logits = tf.matmul(fc2, fc3_W) + fc3_b
 
-    return logits
+    return tf.matmul(fc2, fc3_W) + fc3_b
 
 from sklearn.utils import shuffle
 
 x = tf.placeholder(tf.float32, (None, 32, 32, 1))
 y = tf.placeholder(tf.int32, (None))
+keep_prob = tf.placeholder(tf.float32)
 
 one_hot_y = tf.one_hot(y, 43)
 
 #Training pipeline
 rate = 0.001
 
-logits = LeNet(x)
+logits = LeNet(x, keep_prob)
 cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits, one_hot_y)
 loss_operation = tf.reduce_mean(cross_entropy)
 optimizer = tf.train.AdamOptimizer(learning_rate=rate)
@@ -174,7 +201,7 @@ def evaluate(X_data, y_data):
     sess = tf.get_default_session()
     for offset in range(0, num_examples, BATCH_SIZE):
         batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset: offset+BATCH_SIZE]
-        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y, keep_prob: 1.0})
         total_accuracy = total_accuracy + (accuracy*len(batch_x))
     return total_accuracy / num_examples
 
@@ -190,7 +217,7 @@ with tf.Session() as sess:
         for offset in range(0, num_examples, BATCH_SIZE):
             end = offset + BATCH_SIZE
             batch_x, batch_y = X_train[offset:end], y_train[offset:end]
-            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
+            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y, keep_prob: 0.7})
         validation_accuracy = evaluate(X_validation, y_validation)
         print("EPOCH {} ...".format(i+1))
         print("Validation accuracy = {:.3f}".format(validation_accuracy))
